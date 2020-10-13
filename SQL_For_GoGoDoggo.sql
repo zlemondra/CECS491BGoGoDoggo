a3af9741903dd44e6e6d7c0d2349a03d5063bdaa
zlemondra <44854321+zlemondra@users.noreply.github.com>
2020-10-12 20:35:57 -0700

    SQL and home page update

--- SQL_For_GoGoDoggo.sql
+++ SQL_For_GoGoDoggo.sql
@@ -1,23 +1,27 @@
-create humanUsers (
-    firstName varchar(100), not null
-    lastName varchar(100), not null
-    email varchar(100),not null
-    password varchar (50),not null
-    id varchar (50)not null
+create table humanUsers (
+    firstName varchar(100) not null,
+    lastName varchar(100) not null,
+    email varchar(100) not null,
+    password varchar (50) not null,
+    id varchar (50)not null,
+    primary key(id)
 );
-create dogShelter (
-    shelterName varchar(100), not null
-    locationPoint varchar(100), not null
-    email varchar(100),not null
+create table dogShelter (
+    shelterName varchar(100) not null,
+    locationPoint varchar(100) not null,
+    email varchar(100)not null,
     phone varchar(10),
-    password varchar (50), not null
-    id varchar (50) not null
+    password varchar (50) not null,
+    id varchar (50) not null,
+    primary key(id)
 );
-create dogProfile (
+create table dogprofile (
     dogName varchar(100), 
     gender varchar(100), 
-    breed varchar(100), not null
-    age varchar (50), not null
-    id varchar (50), not null
-    shelterID(50) not null
+    breed varchar(100) not null,
+    age varchar (50) not null,
+    id varchar (50) not null,
+    shelterID varchar(50) not null,
+    primary key(id),
+    foreign key(shelterID) references dogShelter(id)
 );
--- mainscreen/app/src/main/java/com/example/mainscreen/ShelterSignOnButton.java
+++ mainscreen/app/src/main/java/com/example/mainscreen/ShelterSignOnButton.java
@@ -1,5 +1,24 @@
 package com.example.mainscreen;
 
-public class ShelterSignOnButton {
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
+public class ShelterSignOnButton extends Activity implements View.OnClickListener{
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+        Button button = (Button) findViewById(R.id.your_button_id);
+        button.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent(MainActivity.this, target_activity.class);
+        startActivity(intent);
+    }
 }
